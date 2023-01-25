a8f1823a17d4064ca123cb64d7a310b78c4a1047
VanessaAloia <vanessa.aloia@newronika.com>
2023-01-25 12:20:44 +0100

    aggiunta requirements .txt

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import PyQt5
+import matplotlib
 #vanessa prova
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
